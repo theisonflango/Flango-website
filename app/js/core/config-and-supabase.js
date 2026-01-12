3d6d555353df506951c75b8f923e6e3dace4356e
Theis <theis@flango.dk>
2026-01-12 13:14:11 +0100

    Auto deploy 2026-01-12 13:14

--- app/js/core/config-and-supabase.js
+++ app/js/core/config-and-supabase.js
@@ -2,7 +2,7 @@
 import { instrumentSupabase } from './db-instrumentation.js';
 
 // App version - skal matche version.json efter deploy
-3.0.0'3.0.0';
+3.0.1'3.0.1';
 
 const SUPABASE_URL = 'https://jbknjgbpghrbrstqwoxj.supabase.co'; // Dette er din nye URL for Flango-3
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impia25qZ2JwZ2hyYnJzdHF3b3hqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MjIwNjMsImV4cCI6MjA3ODE5ODA2M30.ZMlxQyzmXuy43EcKIN6-eO8pJZs2F6kfDw_cfaks9qQ';
