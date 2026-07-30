e07a6e981572307baf66e7c481eeaf18bfe44202
theisonflango <theisonfilm@gmail.com>
2026-07-30 04:31:49 +0200

    Auto deploy (parents) 2026-07-30 04:31

--- forældre/js/portal-v2.js
+++ forældre/js/portal-v2.js
@@ -2067,7 +2067,7 @@
     if (!planviewReady._p) {
       planviewReady._p = new Promise((resolve, reject) => {
         const el = document.createElement('script');
-        el.src = './js/vendor/planview.js?v=5';
+        el.src = './js/vendor/planview.js?v=6';
         el.onload = resolve;
         el.onerror = () => { planviewReady._p = null; reject(new Error('planview')); };
         document.head.appendChild(el);
