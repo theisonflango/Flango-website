444638286b036744ca266ac8dba25c877efcb574
theisonflango <theisonfilm@gmail.com>
2026-08-26 01:29:48 +0200

    Auto deploy (prod) 2026-08-26 01:29

---
+++ app/assets/download-file-E_RUogmd.js
@@ -0,0 +1,1 @@
+import{p as i}from"./platform-CCXFt42U.js";async function a(o,n,e="text/plain"){return i.saveFile(o,n,e)}typeof window<"u"&&(window.__flangoDownloadFile=a);export{a as downloadFile};
