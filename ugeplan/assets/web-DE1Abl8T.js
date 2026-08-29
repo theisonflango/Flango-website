381f2752aab318881a9cac99588a0425986aa44b
theisonflango <theisonfilm@gmail.com>
2026-08-29 15:06:16 +0200

    Auto deploy (ugeplan) 2026-08-29 15:06

---
+++ ugeplan/assets/web-DE1Abl8T.js
@@ -0,0 +1,1 @@
+import{W as n}from"./EditorScreen-le0ZscIa.js";import"./main-CY01-JZW.js";class t extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{t as AppLauncherWeb};
