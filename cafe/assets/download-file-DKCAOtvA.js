b75dd0222aede80c04ae7124902fc7fa1dcee575
theisonflango <theisonfilm@gmail.com>
2026-08-31 14:07:15 +0200

    Auto deploy (prod) 2026-08-31 14:07

--- cafe/assets/download-file-DKCAOtvA.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./platform-9W_y_fBl.js";async function a(o,n,e="text/plain"){return i.saveFile(o,n,e)}typeof window<"u"&&(window.__flangoDownloadFile=a);export{a as downloadFile};
---
+++ cafe/assets/index-C_poibYS.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Bv3RyyQU.js","assets/index-DFpCa_aP.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./platform-BqkS1OWQ.js";import{r as o}from"./index-DFpCa_aP.js";const i=o("Browser",{web:()=>e(()=>import("./web-Bv3RyyQU.js"),__vite__mapDeps([0,1])).then(r=>new r.BrowserWeb)});export{i as Browser};
