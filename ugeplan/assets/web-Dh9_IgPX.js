5f2008796cae02f8756f7b6330049254b9f2b86d
theisonflango <theisonfilm@gmail.com>
2026-09-06 15:55:16 +0200

    Auto deploy (ugeplan) 2026-09-06 15:55

--- ugeplan/assets/web-Dh9_IgPX.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./EditorScreen-Dgwyu1fE.js";import"./main-DHeVEogJ.js";class t extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{t as AppLauncherWeb};
