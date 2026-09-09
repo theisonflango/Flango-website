a056e5bac2b6abd2e639461f1d110193318b76cd
theisonflango <theisonfilm@gmail.com>
2026-09-09 16:15:09 +0200

    Auto deploy (cafe) 2026-09-09 16:15

--- cafe/assets/download-file-4GvjM9BV.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./platform-Cb1uG_TN.js";async function a(o,n,e="text/plain"){return i.saveFile(o,n,e)}typeof window<"u"&&(window.__flangoDownloadFile=a);export{a as downloadFile};
