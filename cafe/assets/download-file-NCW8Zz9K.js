02440b030bb6dd31a0a613afb0b6a641f8f63c0e
theisonflango <theisonfilm@gmail.com>
2026-09-18 01:13:58 +0200

    Auto deploy (cafe) 2026-09-18 01:13

--- cafe/assets/download-file-NCW8Zz9K.js
+++
@@ -1,1 +0,0 @@
-import{p as i}from"./platform-B82djGUq.js";async function a(o,n,e="text/plain"){return i.saveFile(o,n,e)}typeof window<"u"&&(window.__flangoDownloadFile=a);export{a as downloadFile};
