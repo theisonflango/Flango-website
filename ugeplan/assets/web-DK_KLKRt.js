35b4a50f5c66b4fc9cc2b2c8b9c1b39f3810b1af
theisonflango <theisonfilm@gmail.com>
2026-09-19 23:23:23 +0200

    Auto deploy (ugeplan) 2026-09-19 23:23

--- ugeplan/assets/web-DK_KLKRt.js
+++
@@ -1,1 +0,0 @@
-import{bm as n}from"./main-D4e4VeUR.js";class p extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{p as AppLauncherWeb};
