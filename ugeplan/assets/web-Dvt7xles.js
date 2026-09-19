816dd0b5bd9c814aa794b9c43a09d257be5e784d
theisonflango <theisonfilm@gmail.com>
2026-09-19 23:52:45 +0200

    Auto deploy (ugeplan) 2026-09-19 23:52

--- ugeplan/assets/web-Dvt7xles.js
+++
@@ -1,1 +0,0 @@
-import{bl as n}from"./main-CX2p9Sau.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
