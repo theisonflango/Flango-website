bce66743178641f1da359742fae82407daaf58b2
theisonflango <theisonfilm@gmail.com>
2026-09-20 00:34:08 +0200

    Auto deploy (ugeplan) 2026-09-20 00:34

--- ugeplan/assets/web-CEFR2pu8.js
+++
@@ -1,1 +0,0 @@
-import{bl as n}from"./main-C8ABUCH4.js";class o extends n{async canOpenUrl(e){return{value:!0}}async openUrl(e){return window.open(e.url,"_blank"),{completed:!0}}}export{o as AppLauncherWeb};
